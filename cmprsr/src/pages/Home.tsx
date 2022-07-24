665cdab7e1077d87c73b8b7da136ac6ef8e51140
Lior Pollak <4294489+liorp@users.noreply.github.com>
2022-07-24 16:20:27 +0300

    fix: form submit

--- cmprsr/src/pages/Home.tsx
+++ cmprsr/src/pages/Home.tsx
@@ -3,45 +3,52 @@ import Footer from 'components/Footer'
 import Huffman from 'components/Huffman'
 import LempelZiv from 'components/LempelZiv'
 import type { ReactElement } from 'react'
-import { useRef, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 const encodings = [Huffman, LempelZiv]
 
 export default function Home(): ReactElement {
-	const [toggle, setToggle] = useState(false)
-	const plainTextReference = useRef<HTMLTextAreaElement>(null)
+	const [searchParameters, setSearchParameters] = useSearchParams()
+	const plaintext = searchParameters.get('plaintext')
 	return (
 		<div className='flex h-full flex-col'>
-			<form className='prose mx-auto mt-2 grid w-1/2 gap-1 lg:prose-xl'>
-				<h1 className='text-gradient w-min transform bg-green-300 font-vt323 transition hover:-rotate-6'>
-					CMPRSR
-				</h1>
+			<form
+				className='prose mx-auto mt-2 grid w-1/2 gap-1 lg:prose-xl'
+				onSubmit={(event): void => {
+					setSearchParameters({
+						// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+						plaintext: (event.target as HTMLFormElement).elements.plaintext
+							.value
+					})
+					event.preventDefault()
+				}}
+			>
+				<a href='/'>
+					<h1 className='text-gradient w-min transform bg-green-300 font-vt323 transition hover:-rotate-6'>
+						CMPRSR
+					</h1>
+				</a>
 				<textarea
 					id='plaintext'
+					name='plaintext'
 					className='textarea'
 					placeholder='Enter your text here'
-					ref={plainTextReference}
+					defaultValue={`${plaintext ?? ''}`}
+					required
 				/>
-				<button
-					className='btn'
-					type='submit'
-					onClick={(event: React.MouseEvent<HTMLButtonElement>): void => {
-						setToggle(t => !t)
-						event.preventDefault()
-					}}
-				>
+				<button className='btn' type='submit'>
 					Compress
 				</button>
 			</form>
 			<div className='carousel rounded-box mx-auto w-3/4 grow'>
-				{plainTextReference.current?.value
+				{plaintext
 					? encodings.map((Encoding, index) => (
 							<div
 								// eslint-disable-next-line react/no-array-index-key
-								key={index + Number(toggle)}
+								key={index}
 								className='carousel-item m-2'
 							>
-								<Encoding plainText={plainTextReference.current?.value ?? ''} />
+								<Encoding plainText={plaintext} />
 							</div>
 					  ))
 					: undefined}
